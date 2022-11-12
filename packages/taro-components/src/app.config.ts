b49896830389fd9bc9ee5e6c5889bf2dea2971aa
jcg01423690 <jcg01423690@alibaba-inc.com>
2022-11-12 16:52:11 +0800

    初始化monorepo

---
+++ packages/taro-components/src/app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  components: [
+    'components/Button/index',
+  ],
+  pages: [
+    'pages/index/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+})
